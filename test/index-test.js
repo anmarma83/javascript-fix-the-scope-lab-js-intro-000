a354de19e065dfa36c98280cfaaf196a8ba0f946
Anmar <anmarma83@gmail.com>
2021-02-26 17:01:46 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -65,5 +65,5 @@ var funkyFunction = function() {
  var funkyFunction = function() {
 
 // We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
-var theFunk = funkyFunction
+
 var theFunk = funkyFunction()()
