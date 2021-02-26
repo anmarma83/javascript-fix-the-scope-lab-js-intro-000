db22677dbde5e97bbfe8dcfff16f4e85c3a2b0a9
Anmar <anmarma83@gmail.com>
2021-02-26 17:02:42 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -60,10 +60,9 @@ function add2(n){
   return "NaN";
 }
 var funkyFunction = function() {
-  return function() {
-    return "FUNKY!"
- var funkyFunction = function() {
+    return function(){
+      return "FUNKY!"
+    }
+  }
 
-// We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
-
-var theFunk = funkyFunction()()
+  var theFunk = funkyFunction()();
