2836e0b423990153281416c4dc34c8e793e87d55
Anmar <anmarma83@gmail.com>
2021-02-26 16:09:11 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,5 @@
 /*global describe, it */
-var animal ='Cat';
+var animal ='cat';
 describe('Fix the Scope', function() {
   describe('myAnimal()', function() {
     it('returns the animal variable', () => {
