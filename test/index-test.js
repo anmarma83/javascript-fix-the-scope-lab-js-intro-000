ff6538c4087846a704a0a01c291fd83a66b0a6f7
Anmar <anmarma83@gmail.com>
2021-02-26 17:10:59 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -64,7 +64,6 @@ var funkyFunction = function() {
   }
 }
 
-// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// N: you only need to modify the code below this line.
+
 
 var theFunk = funkyFunction()();
